fbea2ed43721072928140ed3a0c02afde347565b
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2025-11-24 22:47:44 +0200

    fix validation schema

--- src/components/Auth/LoginForm/index.jsx
+++ src/components/Auth/LoginForm/index.jsx
@@ -12,7 +12,7 @@ const validationSchema = Yup.object().shape({
     .email("Please enter a valid email")
     .required("Email is required"),
   password: Yup.string()
-    .min(6, "Password must be at least 6 characters")
+    .min(8, "Password must be at least 8 characters")
     .required("Password is required"),
 });
 
--- src/components/Auth/RegisterForm/index.jsx
+++ src/components/Auth/RegisterForm/index.jsx
@@ -15,7 +15,7 @@ const validationSchema = Yup.object().shape({
     .email("Please enter a valid email")
     .required("Email is required"),
   password: Yup.string()
-    .min(6, "Password must be at least 6 characters")
+    .min(8, "Password must be at least 8 characters")
     .required("Password is required"),
   confirmPassword: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords must match")
